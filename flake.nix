49119f499cf19e9eeb5bc58dfab357a4c048f186
IngilizAdam <ht@hasantuna.com>
2024-03-16 07:46:14 +0300

    sync home-manager version with nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }: 
